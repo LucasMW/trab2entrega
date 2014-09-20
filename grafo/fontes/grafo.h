0ee5d500d8f4b192567df521d347a3f674308c8c
LucasMW <great-lucas@hotmail.com>
2014-09-20 19:57:20 -0300

    Adicionei as especificações dasfunções do grafp

---
+++ grafo/fontes/grafo.h
@@ -0,0 +1,16 @@
+﻿typedef struct GRA_tagGrafo * GRA_tppGrafo ;
+
+typedef enum {
+
+         GRA_CondRetOK ,
+               /* Concluiu corretamente */
+
+         GRA_CondRetGrafoVazio ,
+               /* o grafo não contém elementos */
+
+
+   } GRA_tpCondRet ;
+GRA_tpCondRet GRA_Cria( GRA_tppGrafo* grafo ,int max_nodes );
+GRA_tpCondRet   GRA_InsereNo ( GRA_tppGrafo* grafo, void * pInfo);
+GRA_tpCondRet  GRA_InsereAresta( GRA_tppGrafo* grafo, int node_i, int node_j, float cost, char direction);
+GRA_tpCondRet  GRA_Imprime(GRA_tppGrafo* graph);
